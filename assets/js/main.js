364d9f97cd05cc1cbae61b15d5860d3e4dc5764c
Mikal Morello <mmorel01@dhcp-130-64-12-165.medford.tufts.edu>
2019-05-06 08:34:41 -0400

    JS - Cleaning up code, running through JSHint

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,3 @@
-
 // VARIABLES
 
   const xhr = new XMLHttpRequest(),
@@ -9,15 +8,14 @@
 
   let categoryFlat = [],
       categoryNested = [],
-      categoryFlatResults = '',
       flatNav = '',
       nestedNav = '';
 
 // FUNCTIONS
 
   // API Call
-  function callThatAPI() {
-    xhr.open('GET', `${baseUrl}`)
+  function callAPI() {
+    xhr.open('GET', `${baseUrl}`);
     xhr.send();
     xhr.onload = handleSuccess;
     xhr.onerror = handleError;
@@ -26,7 +24,6 @@
   // API Success
   function handleSuccess() {
     var response = JSON.parse(xhr.responseText);
-    console.log('working');
     loadProfile(response);
   }
 
@@ -35,13 +32,12 @@
     console.log('broken');
   }
 
-
   // Load Results
   function loadProfile(response){
     let profile = response;
     for(let i = 0; i < profile.length; i++) { 
       let profileField = profile[i];
-      // Create array for fields that fall within the General Category
+      // Create arrays for flat and nested fields
       if(profileField.containing_object || profileField.properties){
         categoryNested.push(profileField);
       } else {
@@ -52,7 +48,9 @@
     buildFlatNav(categoryFlat);
     buildNestedNav(categoryNested);
     buildSidebarNav(flatNav, nestedNav);
+    // Build Main Content
     defaultMainContent(categoryFlat);
+    // Menu Actions
     navLinkClick(categoryFlat, categoryNested);
     navLinkSectionClick();
   }
@@ -76,11 +74,11 @@
     for(let i = 0; i < categoryFlat.length; i++) {
       let profileField = categoryFlat[i],
           displayName = addReadableNames(profileField.name);
-      
       flatNav += `
         <li class="nav__link-section-item">
           <a href="#${profileField.id}" class="nav__link-section scrollsection" >${displayName}</a>
-        </li>`;
+        </li>
+      `;
     }
     return flatNav;
   }
@@ -93,7 +91,6 @@
           nestedSubNav = '',
           profileSubFields = profileSubFieldType(profileField),
           parentDisplayName = addReadableNames(profileField.name);
-      
       // loop through category subitems
       for(let i = 0; i < profileSubFields.length; i++) {
         let profileSubField = profileSubFields[i],
@@ -102,7 +99,7 @@
           <li class="nav__link-section-item">
             <a href="#${profileSubField.id}" class="nav__link-section">${displayName}</a>
           </li>
-        `
+        `;
       }
       nestedNav += `
         <li class="nav__link-item">
@@ -110,12 +107,13 @@
           <ul class="nav__link-sections">
             ${nestedSubNav}
           </ul>
-        </li>`
+        </li>
+      `;
     }
     return nestedNav;
   }
 
-  // Create a SidebarNav
+  // Build a SidebarNav
   function buildSidebarNav(flatNav, nestedNav){
     sidebarNavLinks.innerHTML = `
       <li class="nav__link-item">
@@ -125,7 +123,7 @@
         </ul>
       </li>
       ${nestedNav}
-      `;
+    `;
   }
 
   // Build Main Content Section
@@ -140,35 +138,29 @@
         <div class="section__data-description section__data-description--${fieldClass}">
           ${fieldDescription}
         </div>
-      </li>`;
+      </li>
+    `;
     return mainContentSection;
   }
 
 
   // Build Main Content - Nested
   function buildMainContent(category, link) {
-
     if (link.id == 'general') {
       mainTitle.innerHTML = `${link.id}`;
-
       let mainContentSections = mainContentRender(category);
-
       mainContent.innerHTML = `
         ${mainContentSections}
       `;
     } else {
-      // If nested content 
-      // Loop through category elements
+      // If nested content, loop through category elements
       for(let i = 0; i < category.length; i++) {
         let profileField = category[i],
             profileSubFields = profileSubFieldType(profileField);
-        
         if(profileField.name == link.id){
           let mainContentSections = mainContentRender(profileSubFields);
-          
           // set main title
           mainTitle.innerHTML = `${profileField.name}`;
-          
           // set main content
           mainContent.innerHTML = `
             ${mainContentSections}
@@ -183,25 +175,23 @@
   // Main Content
 
   function mainContentRender(fields){
-        let mainContentSections = '';
-    
-        for(let i = 0; i < fields.length; i++) {
-          let profileField = fields[i],
-              displayName = addReadableNames(profileField.name);
-          
-          mainContentSections += `
-          <section id="${profileField.id}" class="section section--main">
-            <div class="section__header">
-              <h3 class="section__title">${displayName}</h3>
-            </div>
-            <ul class="section__data-list">
-              ${buildMainContentSection('data-type', 'Type', profileField.data_type)}
-              ${buildMainContentSection('app-keys', 'Usage', profileField.app_keys)}
-              ${buildMainContentSection('name', 'Evertrue Field Name', profileField.name)}
-            </ul>
-          </section>
-          `;
-        } 
+    let mainContentSections = '';
+    for(let i = 0; i < fields.length; i++) {
+      let profileField = fields[i],
+          displayName = addReadableNames(profileField.name);
+      mainContentSections += `
+      <section id="${profileField.id}" class="section section--main">
+        <div class="section__header">
+          <h3 class="section__title">${displayName}</h3>
+        </div>
+        <ul class="section__data-list">
+          ${buildMainContentSection('data-type', 'Type', profileField.data_type)}
+          ${buildMainContentSection('app-keys', 'Usage', profileField.app_keys)}
+          ${buildMainContentSection('name', 'Evertrue Field Name', profileField.name)}
+        </ul>
+      </section>
+      `;
+    } 
     return mainContentSections;
   }
 
@@ -209,15 +199,12 @@
   function activeLink(link){
     let parent = link.parentNode,
         parentSiblings = getSiblings(parent);
-
     // Remove active link class from siblings
     Array.prototype.forEach.call(parentSiblings, function(element) {
       element.classList.remove('active');
     });
-    
     // Add active link class
     parent.classList.add('active');
-    
     // If category link remove all child active classes
     if (link.classList.contains('nav__link')) {
       let sectionLinks = document.getElementsByClassName('nav__link-section-item');
@@ -228,30 +215,28 @@
     }
   }
 
-    
   // Add readable names to fields
   function addReadableNames(fieldName) {
     let displayName = '';
     switch(fieldName) {
       case 'name_first':
         return displayName = 'first name';
-        break;
       case 'et_donor_index':
         return displayName = 'evertrue donor index';
-        break;
       default:
         return displayName = fieldName.replace(/_/g, ' ');
     }
   }
 
+  // Set default active element on page load
   function defaultMainContent(categoryFlat) {
-    let category = categoryFlat,
-        link = document.getElementById('general');
+    let link = document.getElementById('general');
     buildMainContent(categoryFlat, link);
     // Activate Link
     activeLink(link);
   }
 
+  // Identify Siblings
   var getSiblings = function (elem) {
     // Setup siblings array and get the first sibling
     var siblings = [];
@@ -261,21 +246,21 @@
       if (sibling.nodeType === 1 && sibling !== elem) {
         siblings.push(sibling);
       }
-      sibling = sibling.nextSibling
+      sibling = sibling.nextSibling;
     }
     return siblings;
   };
 
+  // Remove the hash from a url
   function removeHash () { 
     history.pushState("", document.title, window.location.pathname + window.location.search);
   }
   
-
-// EVENT
+// EVENTS
 
   // Sidebar Navigation Category Click
   function navLinkClick(categoryFlat, categoryNested) {
-    let sidebarNavLinks = document.getElementsByClassName('nav__link')
+    let sidebarNavLinks = document.getElementsByClassName('nav__link');
     Array.prototype.forEach.call(sidebarNavLinks, function(link) {
       link.addEventListener('click', function() {
         let category = '';
@@ -293,7 +278,7 @@
 
   // Sidebar Navigation Section Click
   function navLinkSectionClick() {
-      let sidebarNavLinks = document.getElementsByClassName('nav__link-section')
+      let sidebarNavLinks = document.getElementsByClassName('nav__link-section');
       Array.prototype.forEach.call(sidebarNavLinks, function(link) {
         link.addEventListener('click', function() {
           // Activate Section Link
@@ -303,7 +288,7 @@
     }
 
   // Default calls
-  callThatAPI();
+  callAPI();
   
 
 
